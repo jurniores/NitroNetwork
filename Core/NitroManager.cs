d036095a61617870fe76377ffb8bc4a29c4b0ab5
jurniores <jurniores@gmail.com>
2025-05-13 13:38:57 -0300

    Generator Other folder

--- Core/NitroManager.cs
+++ Core/NitroManager.cs
@@ -2,11 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
-using System.Reflection;
-using NitroNetwork.Core;
 using UnityEngine;
-using System.Net;
-using System.Collections;
+
 
 
 #if UNITY_EDITOR
